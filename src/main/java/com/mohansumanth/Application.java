0d6a4699e5affd7e8771b32fa71dd4f8728f98f8
mohansumanth450 <mohan.sumanth450@gmail.com>
2020-04-21 23:29:09 +0530

    Adding java source files

---
+++ src/main/java/com/mohansumanth/Application.java
@@ -0,0 +1,17 @@
+package com.mohansumanth;
+
+@SuppressWarnings("unused")
+public class Application {
+
+	public Application(){
+		System.out.println("Inside Application");
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Starting Application");
+		Application app = new Application();
+		
+	}
+	
+}
